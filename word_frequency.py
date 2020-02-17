f90235f9a7f3cb14508ece274839018df809f72e
dandeboer <ddeboer926@gmail.com>
2020-02-17 14:29:36 -0500

    can remove all punctuation, and uppercase characters from a string. can almost remove all stop words, but cannot remove the first word or the last word in a string if it is a stop word

--- word_frequency.py
+++ word_frequency.py
@@ -1,27 +1,65 @@
+test = "That on the first day of January, in the year of our Lord one thousand eight hundred and sixty-three, all persons held as slaves within any State or designated part of a State, the people whereof shall then be in rebellion against the United States, shall be then, thenceforward, and forever free; and the Executive Government of the United States, including the military and naval authority thereof, will recognize and maintain the freedom of such persons, and will do no act or acts to repress such persons, or any of them, in any efforts they may make for their actual freedom."
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+no_punct = ""
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+for char in test:
+    if char not in punctuations:
+        no_punct = no_punct + char
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+no_case = no_punct.lower()
+
+# jan = "january"
+
+# no_january = no_case.replace(jan, "")
+
+# print(no_january)
+
+no_stop = no_case
+
+for word1 in no_case.split():
+    for word2 in STOP_WORDS:
+        if word1 == word2:
+            print(word1)
+            no_stop = no_stop.replace(" " + word1 + " ", " ")
+
+print(no_stop)
 
 
-if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
 
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
+    
 
-    file = Path(args.file)
-    if file.is_file():
-        print_word_freq(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+
+
+
+
+
+
+
+
+
+
+# def print_word_freq(file):
+#     """Read in `file` and print out the frequency of words in that file."""
+#     pass
+
+
+# if __name__ == "__main__":
+#     import argparse
+#     from pathlib import Path
+
+#     parser = argparse.ArgumentParser(
+#         description='Get the word frequency in a text file.')
+#     parser.add_argument('file', help='file to read')
+#     args = parser.parse_args()
+
+#     file = Path(args.file)
+#     if file.is_file():
+#         print_word_freq(file)
+#     else:
+#         print(f"{file} does not exist!")
+#         exit(1)
